0fcb95a27e316b87ad20166aa076693102a36c4f
charles-ram <ram.charles429@gmail.com>
2023-03-02 11:36:03 -0600

    api-route index.js setup

--- routes/api-route/index.js
+++ routes/api-route/index.js
@@ -1,1 +1,24 @@
 const router = require('express').Router();
+const {createNewNote, updateDB} = require("../../notes/notes");
+const { v4: uuidv4 } = require('uuid');
+const {notes} = require("../../db/db.json");
+
+// Shows all notes in JSON data
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+    const params = req.params.id
+    updateDB(params, notes);
+    res.redirect('');
+});
+
+router.delete("/notes/:id" , (req, res) => {
+    const params = req.params.id
+    updateDB(params, notes);
+    res.redirect('');
+});
+
+module.exports = router;
